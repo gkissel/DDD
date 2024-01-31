7894d66b6c44c583ef0123956cbba305548f6465
Gustavo-Kissel <kisselgustavo@gmail.com>
2024-01-31 08:50:18 -0300

    gkissel : Value ogject slug

--- src/domain/services/answer-question.spec.ts
+++ src/domain/services/answer-question.spec.ts
@@ -1,16 +1,22 @@
 import { beforeEach, expect, it } from 'vitest'
 import { AnswerQuestionService } from './answer-question'
 import { describe } from 'node:test'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/base/answers-repository'
 
 let sut: AnswerQuestionService
 
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {},
+}
+
 describe('AnswerQuestionService', () => {
   beforeEach(() => {
-    sut = new AnswerQuestionService()
+    sut = new AnswerQuestionService(fakeAnswersRepository)
   })
 
-  it('should be able to create an answer', () => {
-    const answer = sut.execute({
+  it('should be able to create an answer', async () => {
+    const answer = await sut.execute({
       questionId: '1',
       instructorId: '1',
       content: 'Nova resposta',
