289b1ae7f123df5b5c7093ab3d49c4f91c31e989
Gustavo-Kissel <kisselgustavo@gmail.com>
2024-02-01 14:23:30 -0300

    gkissel : read notification service

--- src/domain/forum/application/services/delete-answer.spec.ts
+++ src/domain/forum/application/services/delete-answer.spec.ts
@@ -2,7 +2,7 @@ import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-r
 import { makeAnswer } from 'test/factories/make-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { DeleteAnswerService } from './delete-answer'
-import { NotAllowedError } from './errors/not-allowed-error'
+import { NotAllowedError } from '../../../../core/errors/not-allowed-error'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
